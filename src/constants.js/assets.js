181c67756911c1fa27d9891b9a62178380c425f2
Adarsh L K <erlkadarsh@gmail.com>
2025-01-07 18:48:29 +0530

    Update assets file for new projects 🚀

--- src/constants.js/assets.js
+++ src/constants.js/assets.js
@@ -1,4 +1,4 @@
-export { default as logo } from "../assets/logo.png"
+export { default as logo } from "../assets/logo.png";
 export { default as cpp } from "../assets/tech/cpp.png";
 export { default as css } from "../assets/tech/css.png";
 export { default as firebase } from "../assets/tech/firebase.png";
@@ -12,27 +12,29 @@ export { default as python } from "../assets/tech/python.png";
 export { default as reactjs } from "../assets/tech/react.png";
 export { default as tailwind } from "../assets/tech/tailwind.png";
 export { default as vercel } from "../assets/tech/vercel.png";
-export { default as postman } from "../assets/postman.svg"
-export { default as grass } from "../assets/grass.jpg"
-export { default as SkillBuild } from "../assets/Skillsbuild.jpg"
-export { default as external } from "../assets/external.svg"
-export { default as code } from "../assets/code.svg"
-export { default as gmail } from "../assets/gmail.svg"
-export { default as instagram } from "../assets/instagram.svg"
-export { default as linkedin } from "../assets/linkedin.svg"
-export { default as discord } from "../assets/discord.svg"
-export { default as about } from "../assets/about.png"
-export { default as contact } from "../assets/contact.png"
-export { default as more } from "../assets/more.svg"
-export { default as user } from "../assets/user.png"
-export { default as projects } from "../assets/projects.png"
-export { default as tech } from "../assets/tech.png"
-export { default as contact_icon } from "../assets/contact-sidebar.png"
-export { default as experiences } from "../assets/experiences.png"
-export { default as twitter} from "../assets/twitter.png"
-export { default as githublogo} from "../assets/githubhome.svg"
-export {default as FireSink} from "../assets/projects/FireSink.png"
-export {default as Kanban} from "../assets/projects/Kanban.png"
-export {default as Echo} from "../assets/projects/Echo.png"
-export {default as GitFinder} from "../assets/projects/GitFinder.png"
-export {default as garaazLogo} from "../assets/garaazLogo.png"
+export { default as postman } from "../assets/postman.svg";
+export { default as grass } from "../assets/grass.jpg";
+export { default as SkillBuild } from "../assets/Skillsbuild.jpg";
+export { default as external } from "../assets/external.svg";
+export { default as code } from "../assets/code.svg";
+export { default as gmail } from "../assets/gmail.svg";
+export { default as instagram } from "../assets/instagram.svg";
+export { default as linkedin } from "../assets/linkedin.svg";
+export { default as discord } from "../assets/discord.svg";
+export { default as about } from "../assets/about.png";
+export { default as contact } from "../assets/contact.png";
+export { default as more } from "../assets/more.svg";
+export { default as user } from "../assets/user.png";
+export { default as projects } from "../assets/projects.png";
+export { default as tech } from "../assets/tech.png";
+export { default as contact_icon } from "../assets/contact-sidebar.png";
+export { default as experiences } from "../assets/experiences.png";
+export { default as twitter } from "../assets/twitter.png";
+export { default as githublogo } from "../assets/githubhome.svg";
+export { default as FireSink } from "../assets/projects/FireSink.png";
+export { default as Kanban } from "../assets/projects/Kanban.png";
+export { default as Echo } from "../assets/projects/Echo.png";
+export { default as Snipz } from "../assets/projects/Snipz.png";
+export { default as JobSphere } from "../assets/projects/JobSphere.png";
+export { default as GitFinder } from "../assets/projects/GitFinder.png";
+export { default as garaazLogo } from "../assets/garaazLogo.png";
