f9e3d27228249b745a62441b34562b2e927681fa
Adarsh L K <erlkadarsh@gmail.com>
2025-01-07 18:47:33 +0530

    Update the links for new projects 🎯

--- src/constants.js/links.js
+++ src/constants.js/links.js
@@ -1,33 +1,37 @@
 // social accounts
-export const gmailLink = "mailto:erlkadarsh@gmail.com"
-export const linkedinLink = "https://www.linkedin.com/in/adarshlkdev"
-export const instagramLink = "#"
-export const githubLink = "https://github.com/adarshlkdev"
-export const discordLink = "https://discord.com/channels/adarshlkdev"
-export const twitterLink = "https://twitter.com/adarshlkdev"
+export const gmailLink = "mailto:erlkadarsh@gmail.com";
+export const linkedinLink = "https://www.linkedin.com/in/adarshlkdev";
+export const instagramLink = "#";
+export const githubLink = "https://github.com/adarshlkdev";
+export const discordLink = "https://discord.com/channels/adarshlkdev";
+export const twitterLink = "https://twitter.com/adarshlkdev";
 
 // hirer website links
-export const postmanLink = "https://badgr.com/public/assertions/wmyOB0UkSD2J2ZzIpQgmOA?identity__email=adarshvhp3344@gmail.com"
-export const grassLink = "https://drive.google.com/file/d/1qs8cjJz4nUjjU-k0mZeKt7lCw3cL_8C6/view?pli=1"
-export const SkillBuildLink = "https://drive.google.com/file/d/1glrcUj7TkQCK0DOlrDOQL5vIwnVmjJab/view"
-export const garaazLink = "https://drive.google.com/file/d/1mcl0ThXRGQghbov4SMwAdNNxAqLnuWq4/view?usp=sharing"
+export const postmanLink =
+  "https://badgr.com/public/assertions/wmyOB0UkSD2J2ZzIpQgmOA?identity__email=adarshvhp3344@gmail.com";
+export const grassLink =
+  "https://drive.google.com/file/d/1qs8cjJz4nUjjU-k0mZeKt7lCw3cL_8C6/view?pli=1";
+export const SkillBuildLink =
+  "https://drive.google.com/file/d/1glrcUj7TkQCK0DOlrDOQL5vIwnVmjJab/view";
+export const garaazLink =
+  "https://drive.google.com/file/d/1mcl0ThXRGQghbov4SMwAdNNxAqLnuWq4/view?usp=sharing";
 
 // projects links
 export const projectsInfo = {
-    GitFinder: {
-        visit: "https://gitfinder-adarshlkdev.vercel.app/",
-        github: "https://github.com/adarshlkdev/GitFinder"
-    },
-    FireSink: {
-        visit: "https://firesink.vercel.app/",
-        github: "https://github.com/adarshlkdev/Firesink"
-    },
-    Kanbanize: {
-        visit: "https://kanbanize-board.vercel.app/",
-        github: "https://github.com/adarshlkdev"
-    },
-    Echo: {
-        visit: "https://echo-nine-phi.vercel.app/",
-        github: "https://github.com/adarshlkdev"
-    }
-}
+  GitFinder: {
+    visit: "https://gitfinder-adarshlkdev.vercel.app/",
+    github: "https://github.com/adarshlkdev/GitFinder",
+  },
+  FireSink: {
+    visit: "https://firesink.vercel.app/",
+    github: "https://github.com/adarshlkdev/Firesink",
+  },
+  Snipz: {
+    visit: "https://snipz.onrender.com/",
+    github: "https://github.com/adarshlkdev",
+  },
+  JobSphere: {
+    visit: "https://jobsphere-adarshlkdev.onrender.com/",
+    github: "https://github.com/adarshlkdev",
+  },
+};
